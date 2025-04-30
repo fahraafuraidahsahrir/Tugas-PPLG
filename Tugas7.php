fb9db2a10d53fa5fc82b765383af2fc3836e724a
fahraafuraidahsahrir <fahrah08@gmail.com>
2025-04-30 15:22:56 +0800

    Perubahan Tugas 7

--- Tugas7.php
+++ Tugas7.php
@@ -1,10 +1,10 @@
 <?php
-$kondisi_akifah = "sehat";
-$kondisi_wali_akifah = "sehat";
+$kondisi_akifah = true;
+$kondisi_wali_akifah = false;
 
-if ($kondisi_akifah == "sehat" && $kondisi_wali_akifah == "sehat") {
-    echo "Akifah berangkat ke sekolah";
+if ($kondisi_akifah && $kondisi_wali_akifah) {
+    echo "Maka Akifah berangkat ke sekolah";
 } else {
-    echo "Akifah tidak berangkat ke sekolah";
+    echo " Maka Akifah tidak berangkat ke sekolah";
 }
 ?>
