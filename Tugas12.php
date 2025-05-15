09b1fbdd985a6d156442ca1261a2e01ec3806d0d
fahraafuraidahsahrir <fahrah08@gmail.com>
2025-05-15 15:35:26 +0800

    Tugas PPLG

---
+++ Tugas12.php
@@ -0,0 +1,21 @@
+<?php
+$Nama_Anggota = [
+    "Akifah Zahrani Arafat",
+    "Al Qumaerah",
+    "Althaf Azza Zahra",
+    "Andi Alfirqahtunnajiyah Ahsyar",
+    "Andi Mufrodatul fanzila",
+    "Fahrah Furaidah Sahrir IPJ",
+    "Fauziyyah",
+    "Jilan Fathiyyah",
+    "Nadia Mirzayanti Hasbi",
+    "Rabi'atul Adawiyah",
+    "Alifah Izzah Rahmadini",
+];
+$Nama_Anggota [] = "Muftihaturrahmah";
+$Nama_Anggota [] = "Auliyah Nastity Khayla Armada";
+$Nama_Anggota [] = "Putri Kinanti Al Magfirah";
+echo "Daftar Nama Anggota Kelas 10 B </br></br>";
+foreach ($Nama_Anggota as $Daftar_Nama_Anggota_Kelas_10_B) {
+    echo "-" . $Daftar_Nama_Anggota_Kelas_10_B . "</br></br>";
+}
